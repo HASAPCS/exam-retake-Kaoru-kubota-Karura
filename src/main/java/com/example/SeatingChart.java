b1121c3e28346943415cd13b5d0245e2b58eec6a
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2024-01-03 13:29:10 +0800

    ver. 1.0

--- src/main/java/com/example/SeatingChart.java
+++ src/main/java/com/example/SeatingChart.java
@@ -1,21 +1,53 @@
-// package com.example;
+package com.example;
 
-// import java.util.List;
+import java.util.List;
 
-// public class SeatingChart {
-//     private Student[][] seats;
+public class SeatingChart {
+    private Student[][] seats;
 
-//     // Constructor to implement a)
-//     public SeatingChart(List<Student> studentList, int rows, int cols) {
-//         // Your Implementation here
-//     }
+    // Constructor to implement a)
+    public SeatingChart(List<Student> studentList, int rows, int cols) {
+        // Your Implementation here
+        seats = new Student[rows][cols];
 
-//     // Method to implement b)
-//     public int removeAbsentStudents(int allowedAbsences) {
-//         // Your Implementation here
-//     }
+        int NoName = 0;
 
-//     public Student[][] getSeats() {
-//         return seats;
-//     }
-// }
+        for(int c = 0; c < seats[0].length; c++)
+        {
+            for(int r = 0; r < seats.length; r++)
+            {
+                if(NoName < studentList.size())
+                {
+                    seats[r][c] = studentList.get(NoName);
+                    NoName++;
+                }
+            }
+        }
+    }
+
+    // Method to implement b)
+    public int removeAbsentStudents(int allowedAbsences) {
+        // Your Implementation here
+        int removed = 0;
+
+        for(int r = 0; r < seats.length; r++)
+        {
+            for(int c = 0; c < seats[0].length; c++)
+            {
+                if(seats[r][c] != null && seats[r][c].getAbsenceCount() > allowedAbsences)
+                {
+                    seats[r][c] = null;
+                    removed++;
+                }
+            }
+        }
+        return removed;
+    }
+
+    public Student[][] getSeats() {
+        return seats;
+    }
+}
+
+//
+// tired _(:3」∠)_
