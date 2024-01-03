33315c9682bb940a9857e14a9f1f669d00d915a4
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2024-01-03 13:05:04 +0800

    ver.β

--- src/main/java/com/example/FuelDepot.java
+++ src/main/java/com/example/FuelDepot.java
@@ -1,32 +1,49 @@
-// package com.example;
+package com.example;
+import java.util.List;
 
-// import java.util.List;
+public class FuelDepot {
+    /** The robot used to move the filling mechanism */
+    private FuelRobot filler;
 
-// public class FuelDepot {
-//     /** The robot used to move the filling mechanism */
-//     private FuelRobot filler;
+    /** The list of fuel tanks */
+    private List<FuelTank> tanks;
 
-//     /** The list of fuel tanks */
-//     private List<FuelTank> tanks;
+    /**
+     * Constructs a FuelDepot object.
+     *
+     * @param filler The robot used to move the filling mechanism.
+     * @param tanks  The list of fuel tanks.
+     */
+    public FuelDepot(FuelRobot filler, List<FuelTank> tanks) {
+        this.filler = filler;
+        this.tanks = tanks;
+    }
 
-//     /**
-//      * Constructs a FuelDepot object.
-//      *
-//      * @param filler The robot used to move the filling mechanism.
-//      * @param tanks  The list of fuel tanks.
-//      */
-//     public FuelDepot(FuelRobot filler, List<FuelTank> tanks) {
-//         this.filler = filler;
-//         this.tanks = tanks;
-//     }
+    // Method to implement for a)
+    public int nextTankToFill(int threshold) {
+        // Your implementation here
+        int tankWithLeast = 0;
+    
+        for (int i = 1; i < tanks.size(); i++)
+            if (tanks.get(i).getFuelLevel() < tanks.get(tankWithLeast).getFuelLevel())
+                tankWithLeast = i;
+    
+        if (tanks.get(tankWithLeast).getFuelLevel() <= threshold)
+            return tankWithLeast;
+        else
+            return filler.getCurrentIndex();
+    }
 
-//     // Method to implement for a)
-//     public int nextTankToFill(int threshold) {
-//         // Your implementation here
-//     }
+    // Method to implement for b)
+    public void moveToLocation(int locIndex) {
+        // Your implementation here
+        if (locIndex == filler.getCurrentIndex())
+        return;
 
-//     // Method to implement for b)
-//     public void moveToLocation(int locIndex) {
-//         // Your implementation here
-//     }
-// }
+        if ((locIndex < filler.getCurrentIndex() && filler.isFacingRight())
+                || (locIndex > filler.getCurrentIndex() && !filler.isFacingRight()))
+            filler.changeDirection();
+
+        filler.moveForward(Math.abs(locIndex - filler.getCurrentIndex()));
+    }
+}
